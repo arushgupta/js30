34585060c9abb0d56fc067101344db1a2ca32782
Arush Gupta <arushgupta.dev@gmail.com>
2021-01-18 23:40:28 +0530

    drumkit ready to be played

---
+++ 01 - Drum Kit/drumkit.js
@@ -0,0 +1,15 @@
+function play(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const playedKey = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; // stop function from running
+    audio.currentTime = 0; // reset the sound to allow for quick playing
+    audio.play();
+    playedKey.classList.add('playing');
+}
+function removeClass(e) {
+    if (e.propertyName !== 'transform') return; // to prevent class removal for other property changes
+    this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeClass));
+window.addEventListener('keydown', play());
