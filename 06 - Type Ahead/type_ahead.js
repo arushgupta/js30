01db98d03de732504431b7d0931c5a6fa8591263
Arush Gupta <arushgupta.dev@gmail.com>
2021-01-24 10:35:43 +0530

    added meta tag and a change eventListener

--- 06 - Type Ahead/type_ahead.js
+++ 06 - Type Ahead/type_ahead.js
@@ -1,4 +1,5 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
 const cities = [];
 // fetch() returns a Promise
 // We can use then() on Promise to start working on the raw data that has been returned
@@ -7,15 +8,18 @@ const cities = [];
 const blob = fetch(endpoint)
     .then(blob => blob.json())
     .then(data => cities.push(...data));
+
 function findMatches(wordToMatch, cities) {
     return cities.filter(place => {
         const regex = new RegExp(wordToMatch, 'gi');
         return place.city.match(regex) || place.state.match(regex);
     });
 }
+
 function numberWithCommas(a) {
     return a.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
+
 function displayMatches() {
     const matches = findMatches(this.value, cities);
     const regex = new RegExp(this.value, 'gi');
@@ -29,7 +33,9 @@ function displayMatches() {
     }).join('');
     suggestions.innerHTML = html;
 }
+
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
-//searchInput.addEventListener('change', displayMatches);
+
+searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
