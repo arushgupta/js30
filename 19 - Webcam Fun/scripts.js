ed9397155e333375e48e38f07f044a9efecaee47
Arush Gupta <arushgupta.dev@gmail.com>
2021-02-02 15:14:09 +0530

    implemented image download and filters

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,6 +26,18 @@ function paintToCanvas() {
     // Take an image from webcam and paint on the canvas
     setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        // take pixel data out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // apply the effect
+        //pixels = redEffect(pixels);
+
+        pixels = rgbSplit(pixels);
+        ctx.globalAlpha = 0.1;
+
+        //pixels = greenScreen(pixels);
+
+        // re-assigning the context the edited pixels
+        ctx.putImageData(pixels, 0 , 0);
     }, 16);
 }
 
@@ -38,8 +50,49 @@ function takePhoto() {
     const link = document.createElement('a');
     link.href = data;
     link.setAttribute('download', 'handsome');
-    link.textContent = 'Download Image';
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
     strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i+= 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; // RED
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // BLUE
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for(let i = 0; i < pixels.data.length; i+= 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // RED
+        pixels.data[i + 100] = pixels.data[i + 1]; // GREEN
+        pixels.data[i - 150] = pixels.data[i + 2]; // BLUE
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+    for (i = 0; i < pixels.data.length; i+= 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin &&
+            green >= levels.gmin &&
+            blue >= levels.bmin &&
+            red <= levels.rmax &&
+            green <= levels.gmax &&
+            blue <= levels.bmax) {
+            pixels.data[i + 3] = 0;
+        }
+    }
+    return pixels;
 }
 
 getVideo();
