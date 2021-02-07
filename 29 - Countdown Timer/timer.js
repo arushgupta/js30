41433a816cc6f566723ef95bf3abf50fb31d630b
Arush Gupta <arushgupta.dev@gmail.com>
2021-02-07 18:37:24 +0530

    added functionality to buttons and form

--- 29 - Countdown Timer/timer.js
+++ 29 - Countdown Timer/timer.js
@@ -1,8 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+    // clear any existing timers
+    clearInterval(countdown);
+
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
@@ -37,3 +41,15 @@ function displayEndTime(timestamp) {
     const adjustedHour = hour > 12 ? hour - 12 : hour;
     endTime.textContent = `Be Back At ${adjustedHour < 10 ? '0' : ''}${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}${hour >= 12 ? 'pm' : 'am'}`;
 }
+
+function startTimer() {
+    timer(parseInt(this.dataset.time));
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
+});
